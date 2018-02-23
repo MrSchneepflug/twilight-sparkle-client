ae4166e56b32e9ab790f88f6d44affc64b6a4d70
Johannes Jambor <jambor@billiger-mietwagen.de>
2018-02-23 15:00:22 +0100

    WIP: add current implementation of redux for mobile-client

--- src/index.js
+++ src/index.js
@@ -1,25 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import {Provider} from "react-redux";
-import configureStore from "./TVApp/store";
+import configureMobileStore from "./MobileApp/store";
+import configureTVStore from "./TVApp/store";
 import TVApp from "./TVApp/TVApp";
 import MobileApp from "./MobileApp/MobileApp";
 import "typeface-roboto";
 
-let rootComponent = null;
-const tvStore = configureStore({
+const mobileStore = configureMobileStore({
+  connected: false,
+  team: null,
+  developer: null,
+  estimation: null,
+  selectedDevelopers: []
+});
+
+const tvStore = configureTVStore({
   connected: false,
   estimationsByDeveloper: {}
 });
 
-if (window.device === "mobile") {
-  rootComponent = <MobileApp/>;
-} else {
-  rootComponent = (
-    <Provider store={tvStore}>
-      <TVApp/>
+ReactDOM.render((
+    <Provider store={window.device === "mobile" ? mobileStore : tvStore}>
+      {window.device === "mobile" ? <MobileApp/> : <TVApp/>}
     </Provider>
-  );
-}
-
-ReactDOM.render(rootComponent, document.getElementById('root'));
+  ),
+  document.getElementById('root')
+);
