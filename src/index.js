cdc45d081266f142742c9cd41bf7e932237817c8
Thomas Kolar <kolar@billiger-mietwagen.de>
2018-02-23 13:27:43 +0100

    feat: Add user agent detection

--- src/index.js
+++ src/index.js
@@ -4,9 +4,13 @@ import TVApp from "./TVApp/TVApp";
 import MobileApp from "./MobileApp/MobileApp";
 import "typeface-roboto";
 
+const isMobile = () => {
+  return navigator.userAgent.match(
+    /(iPhone|iPod|iPad|blackberry|android|Kindle|htc|lg|midp|mmp|mobile|nokia|opera mini|palm|pocket|psp|sgh|smartphone|symbian|treo mini|Playstation Portable|SonyEricsson|Samsung|MobileExplorer|PalmSource|Benq|Windows Phone|Windows Mobile|IEMobile|Windows CE|Nintendo Wii)/i
+  );
+};
+
 ReactDOM.render(
-  window.device === "mobile"
-    ? <MobileApp/>
-    : <TVApp/>,
-  document.getElementById('root')
+isMobile() ? <MobileApp /> : <TVApp />,
+  document.getElementById("root")
 );
