32325427f1ef2da8f32f3fd7f06799accb6b6ff5
Johannes Jambor <jambor@billiger-mietwagen.de>
2018-02-23 14:01:10 +0100

    use redux to handle state of TV-client

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {Provider} from "react-redux";
+import configureStore from "./TVApp/store";
 import TVApp from "./TVApp/TVApp";
 import MobileApp from "./MobileApp/MobileApp";
 import "typeface-roboto";
 
-ReactDOM.render(
-  window.device === "mobile"
-    ? <MobileApp/>
-    : <TVApp/>,
-  document.getElementById('root')
-);
+let rootComponent = null;
+const tvStore = configureStore({
+  connected: false,
+  estimationsByDeveloper: {}
+});
+
+if (window.device === "mobile") {
+  rootComponent = <MobileApp/>;
+} else {
+  rootComponent = (
+    <Provider store={tvStore}>
+      <TVApp/>
+    </Provider>
+  );
+}
+
+ReactDOM.render(rootComponent, document.getElementById('root'));
