13fada3cf417cfb42b2564bc34cc61154c3cbefc
Johannes Jambor <jambor@billiger-mietwagen.de>
2018-02-26 19:20:24 +0100

    move dispatch of team-selection to Team component

--- src/MobileApp/Team.js
+++ src/MobileApp/Team.js
@@ -1,20 +1,37 @@
-import React from "react";
+import React, {Component} from "react";
+import {connect} from "react-redux";
+import selectTeam from "./actions/selectTeam";
 
-const Team = ({teamSelectionHandler, name}) => {
-  const clickHandler = () => {
-    teamSelectionHandler(name);
-  };
+class Team extends Component {
+  constructor(props) {
+    super(props);
 
-  const style = {
-    textAlign: "center",
-    padding: "20px"
-  };
+    this.clickHandler = this.clickHandler.bind(this);
+  }
 
-  return (
-    <div style={style} onClick={clickHandler}>
-      <strong>{name}</strong>
-    </div>
-  );
-};
+  clickHandler() {
+    this.props.selectTeam(this.props.name);
+  }
 
-export default Team;
+  render() {
+    const style = {
+      textAlign: "center",
+      padding: "20px"
+    };
+
+    return (
+      <div style={style} onClick={this.clickHandler}>
+        <strong>{this.props.name}</strong>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+});
+
+const mapDispatchToProps = dispatch => ({
+  selectTeam: team => dispatch(selectTeam(team))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Team);
--- src/MobileApp/TeamSelection.js
+++ src/MobileApp/TeamSelection.js
@@ -1,31 +1,14 @@
 import React, {Component} from "react";
-import {connect} from "react-redux";
 import Team from "./Team";
-import {selectTeam} from "./actions/index";
 
 const teams = ["Alpakka", "Einhorn", "Irbis", "Kea", "Raccoon", "Tapir"];
 
-class TeamSelection extends Component {
-  renderTeamSelections() {
-    return teams.map(team => {
-      return <Team teamSelectionHandler={() => this.props.selectTeam(team)} name={team} key={team}/>
-    });
-  };
+const TeamSelection = () => {
+  return (
+    <div>
+      {teams.map(team => <Team name={team} key={team}/>)}
+    </div>
+  );
+};
 
-  render() {
-    return (
-      <div>
-        {this.renderTeamSelections()}
-      </div>
-    );
-  }
-}
-
-const mapStateToProps = state => ({
-});
-
-const mapDispatchToProps = dispatch => ({
-  selectTeam: team => dispatch(selectTeam(team))
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(TeamSelection);
+export default TeamSelection;
