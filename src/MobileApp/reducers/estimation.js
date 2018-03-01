869e6ca97360bb0e201b013edd1a0129fa43b179
Johannes Jambor <jambor@billiger-mietwagen.de>
2018-03-01 19:16:46 +0100

    do not reset estimation after developer-reset

--- src/MobileApp/reducers/estimation.js
+++ src/MobileApp/reducers/estimation.js
@@ -2,8 +2,6 @@ export default function estimation(state = null, action) {
   switch (action.type) {
     case "SELECT_ESTIMATION":
       return action.estimation;
-    case "RESET_DEVELOPER_SELECTION":
-      return null;
     default:
       return state;
   }
