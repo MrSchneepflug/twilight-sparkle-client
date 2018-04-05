ab148275c4fdc721d13251ed5e8397dc9c28fd90
Johannes Jambor <jambor@billiger-mietwagen.de>
2018-04-05 08:09:06 +0200

    add loading-screen for TV-App

--- src/TVApp/index.js
+++ src/TVApp/index.js
@@ -3,9 +3,14 @@ import {connect} from "react-redux";
 import {push} from "../shared/actions/history";
 import Pathname from "../shared/Pathname";
 import * as Scenes from "./scenes";
+import * as SharedScenes from "../shared/scenes";
 
 class TVApp extends Component {
   render() {
+    if (!this.props.connected) {
+      return <SharedScenes.Loading/>;
+    }
+
     const pathname = new Pathname(this.props.location.pathname);
 
     if (pathname.matchesDashboard()) {
@@ -29,12 +34,13 @@ class TVApp extends Component {
 }
 
 const mapStateToProps = state => ({
+  connected: state.connected,
   location: state.location,
   clients: state.clients
 });
 
 const mapDispatchToProps = dispatch => ({
-  redirectToEstimations: () => dispatch(push({ pathname: "/estimations" }))
+  redirectToEstimations: () => dispatch(push({pathname: "/estimations"}))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(TVApp);
