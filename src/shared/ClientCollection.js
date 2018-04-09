666ae9031a39dcf424fbcc71e9bbb4c278059f5d
Johannes Jambor <jambor@billiger-mietwagen.de>
2018-04-09 06:23:38 +0200

    extend ClientCollection to "rate" estimations

--- src/shared/ClientCollection.js
+++ src/shared/ClientCollection.js
@@ -1,3 +1,5 @@
+import {map} from "lodash";
+
 class ClientCollection {
   constructor(clients) {
     this.clients = clients;
@@ -5,6 +7,18 @@ class ClientCollection {
 
   haveEstimated() {
     return this.clients.reduce((result, client) => result && client.estimation, true);
+  }
+
+  haveEstimatedCloseEnough() {
+    return !this.haveEstimatedWithinHighRange();
+  }
+
+  haveEstimatedWithinHighRange() {
+    const estimations = map(this.clients, "estimations");
+    const lowestEstimation = Math.min(...estimations);
+    const highestEstimation = Math.max(...estimations);
+
+    return highestEstimation - lowestEstimation > 2;
   }
 }
 
